f9ca4badf1cf1ab8dc627417e4351cf6a4b03962
Mouli Bheemaneti <moulibheemaneti99@gmail.com>
2026-05-23 12:07:51 +0530

    project init

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,5 @@
 <template>
-  <NuxtPage />
+   <div>
+      Hi
+   </div>
 </template>
--- app/components/AppHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <header class="app-header">
-    <p class="subtitle">॥ Shree Ganeshaya Namah ॥</p>
-    <h1>Mama <span>Parichay</span></h1>
-    <p>A beautiful biodata maker for auspicious alliances</p>
-  </header>
-</template>
